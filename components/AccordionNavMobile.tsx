97cc31974495718a1458276a3ab97213da734e76
KD-Web-Design <contact.kd.webdesign@gmail.com>
2024-10-31 15:13:44 +0100

    Accordion

--- components/AccordionNavMobile.tsx
+++ components/AccordionNavMobile.tsx
@@ -4,7 +4,15 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { navigation } from "@/data/navigationData";
+import {
+  navigation,
+  acheterCategories,
+  vendreCategories,
+  laCoteCategories,
+  vousConseillerCategories,
+} from "@/data/navigationData";
+import Image from "next/image";
+import Link from "next/link";
 
 export function AccordionNavMobile() {
   return (
@@ -18,8 +26,147 @@ export function AccordionNavMobile() {
           >
             {item.name}
           </AccordionTrigger>
-          <AccordionContent>
-            <p>empty</p>
+          <AccordionContent className="">
+            {item.name === "Acheter" && (
+              <div className="flex flex-col gap-1">
+                {acheterCategories.map((category, catIndex) => (
+                  <Link
+                    key={catIndex}
+                    href="#"
+                    className="flex flex-col my-2 px-6 gap-1 w-fit"
+                  >
+                    {category.Icon && (
+                      <div
+                        className={`flex items-center ${
+                          category.name === "Voitures occasions"
+                            ? "text-red-500"
+                            : "hover:text-gray-600"
+                        }`}
+                      >
+                        <category.Icon className="mr-2 h-4 w-4" />
+                        <h3 className="font-semibold">{category.name}</h3>
+                      </div>
+                    )}
+
+                    <span className="text-xs text-gray-500">
+                      {category.description}
+                    </span>
+                  </Link>
+                ))}
+                <Image
+                  src="/images/acheter-image.png"
+                  alt="acheter-image"
+                  width={700}
+                  height={700}
+                  className="my-2 w-full"
+                />
+              </div>
+            )}
+            {item.name === "Vendre" && (
+              <div className="flex flex-col gap-1">
+                {vendreCategories.map((category, catIndex) => (
+                  <Link
+                    key={catIndex}
+                    href="#"
+                    className="flex flex-col my-2 gap-1 w-fit"
+                  >
+                    {category.Icon && (
+                      <div
+                        className={`flex items-center ${
+                          category.name === "Voitures occasions"
+                            ? "text-red-500"
+                            : "hover:text-gray-600"
+                        }`}
+                      >
+                        <category.Icon className="mr-2 h-4 w-4" />
+                        <h3 className="font-semibold">{category.name}</h3>
+                      </div>
+                    )}
+
+                    <span className="text-xs text-gray-500">
+                      {category.description}
+                    </span>
+                  </Link>
+                ))}
+                <Image
+                  src="/images/vendre-image.png"
+                  alt="vendre-image"
+                  width={700}
+                  height={700}
+                  className="my-2 w-full"
+                />
+              </div>
+            )}
+            {item.name === "La Cote" && (
+              <div className="flex flex-col gap-1">
+                {laCoteCategories.map((category, catIndex) => (
+                  <Link
+                    key={catIndex}
+                    href="#"
+                    className="flex flex-col my-2 gap-1 w-fit"
+                  >
+                    {category.Icon && (
+                      <div
+                        className={`flex items-center ${
+                          category.name === "Voitures occasions"
+                            ? "text-red-500"
+                            : "hover:text-gray-600"
+                        }`}
+                      >
+                        <category.Icon className="mr-2 h-4 w-4" />
+                        <h3 className="font-semibold">{category.name}</h3>
+                      </div>
+                    )}
+
+                    <span className="text-xs text-gray-500">
+                      {category.description}
+                    </span>
+                  </Link>
+                ))}
+                <Image
+                  src="/images/lacote-image.png"
+                  alt="lacote-image"
+                  width={700}
+                  height={700}
+                  className="my-2 w-full"
+                />
+              </div>
+            )}
+            {item.name === "Vous conseiller" && (
+              <div className="flex flex-col gap-1">
+                {vousConseillerCategories.map((category, catIndex) => (
+                  <Link
+                    key={catIndex}
+                    href="#"
+                    className="flex flex-col my-2 gap-1 w-fit"
+                  >
+                    {category.Icon && (
+                      <div
+                        className={`flex items-center ${
+                          category.name === "Voitures occasions"
+                            ? "text-red-500"
+                            : "hover:text-gray-600"
+                        }`}
+                      >
+                        <category.Icon className="mr-2 h-4 w-4" />
+                        <h3 className="font-semibold">{category.name}</h3>
+                      </div>
+                    )}
+
+                    <span className="text-xs text-gray-500">
+                      {category.description}
+                    </span>
+                  </Link>
+                ))}
+                <Image
+                  src="/images/vous-conseiller-image.png"
+                  alt="vous-conseiller-image"
+                  width={700}
+                  height={700}
+                  className="my-2 w-full"
+                />
+              </div>
+            )}
           </AccordionContent>
         </AccordionItem>
       ))}
--- data/navigationData.ts
+++ data/navigationData.ts
@@ -1,13 +1,123 @@
+import {
+  Car,
+  CircleHelp,
+  LucideIcon,
+  MessageCircleQuestion,
+  NotepadText,
+  PencilLine,
+  Sparkles,
+  StickyNote,
+  Truck,
+  UserRoundMinus,
+  Wind,
+} from "lucide-react";
 
 interface NavigationItem {
-    name: string;
-    href: string;
-    current?: boolean;
+  name: string;
+  description?: string;
+  href: string;
+  current?: boolean;
+  Icon?: LucideIcon;
 }
 
 export const navigation: NavigationItem[] = [
-    { name: "Acheter", href: "#", current: true },
-    { name: "Vendre", href: "#" },
-    { name: "La Cote", href: "#" },
-    { name: "Vous conseiller", href: "#" },
+  { name: "Acheter", href: "#", current: true },
+  { name: "Vendre", href: "#" },
+  { name: "La Cote", href: "#" },
+  { name: "Vous conseiller", href: "#" },
+];
+
+export const acheterCategories: NavigationItem[] = [
+  {
+    name: "Voitures occasions",
+    description: "Découvrez toutes nos meilleures offres",
+    href: "#",
+    current: true,
+    Icon: Car,
+  },
+  {
+    name: "Voitures neuves",
+    description: "Notre meilleure sélection de véhicules",
+    href: "#",
+    Icon: Sparkles,
+  },
+  {
+    name: "Utilitaires occasions",
+    description: "Trouvez l'utilitaire qui vous correspond",
+    href: "#",
+    Icon: Truck,
+  },
+  {
+    name: "Motos occasions",
+    description: "Un large choix de motos, scooters, quads",
+    href: "#",
+    Icon: Wind,
+  },
+];
+
+export const vendreCategories: NavigationItem[] = [
+  {
+    name: "Vendez à un particulier",
+    description: "Déposez gratuitement votre annonce",
+    href: "#",
+    Icon: PencilLine,
+  },
+  {
+    name: "Vendez à un professionnel",
+    description: "Vendez votre véhicule en 48h",
+    href: "#",
+    Icon: UserRoundMinus,
+  },
+];
+export const laCoteCategories: NavigationItem[] = [
+  {
+    name: "Cote voiture",
+    description: "Estimez la valeur d'un véhicule",
+    href: "#",
+    Icon: Car,
+  },
+  {
+    name: "Cote utilitaire",
+    description: "Estimez la valeur d'un utilitaire",
+    href: "#",
+    Icon: Truck,
+  },
+  {
+    name: "Cote moto",
+    description: "Estimez la valeur d'un deux roues",
+    href: "#",
+    Icon: Wind,
+  },
+];
+export const vousConseillerCategories: NavigationItem[] = [
+  {
+    name: "IA La Centrale",
+    description: "Testez votre nouvelle recherche auto",
+    href: "#",
+    Icon: MessageCircleQuestion,
+  },
+  {
+    name: "Trouvez un professionnel",
+    description: "Explorez l'annuaire des professionnels",
+    href: "#",
+    Icon: UserRoundMinus,
+  },
+  {
+    name: "Fiches techniques",
+    description: "Retrouvez toutes les infos d'un véhicule",
+    href: "#",
+    Icon: StickyNote,
+  },
+  {
+    name: "Conseils",
+    description: "Découvrez nos conseils pour votre projet",
+    href: "#",
+    Icon: NotepadText,
+  },
+  {
+    name: "FAQ",
+    description: "Toutes les réponses à vos questions",
+    href: "#",
+    Icon: CircleHelp,
+  },
 ];
