127eb2dde0e2fb69e633c58efea3f86c3b7a15c0
KD-Web-Design <contact.kd.webdesign@gmail.com>
2024-11-21 15:52:43 +0100

    correction bug

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <main className="lg:p-4 lg:pb-0">
         <HeroBadge />
         <MainSection />
-        <div className="sticky top-0 bg-white z-50 grid grid-cols-2 gap-2 lg:hidden mt-4 px-4 py-2">
+        <div className="sticky top-0 bg-white grid grid-cols-2 gap-2 lg:hidden mt-4 px-4 py-2 z-30">
           <FilterBtn />
           <SortBtn />
         </div>
