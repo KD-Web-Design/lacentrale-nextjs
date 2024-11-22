0b6ff19f820c2956d0ce7230f8015aeb9240a58f
KD-Web-Design <contact.kd.webdesign@gmail.com>
2024-11-22 12:02:48 +0100

    fixed mergeconflict

--- components/mobile/MobileMainSection.tsx
+++ components/mobile/MobileMainSection.tsx
@@ -10,7 +10,7 @@ export default function MobileMainSection() {
     <section className="flex flex-col gap-4 bg-gray-100 p-4 mt-4 lg:hidden">
       <section className="grid sm:grid-cols-2 gap-4 px-4">
         {Array.from({ length: 26 }, (_, index) => (
-          <MobileVehicleCard key={index} />
+          <MobileVehicleCard key={index} index={index} />
         ))}
       </section>
       <SaveSearchBtn />
